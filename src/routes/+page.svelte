54aa8e2e74e702806168c8001cf3fdd765f5d740
Fernando Dodino <fernando.dodino@gmail.com>
2024-11-12 23:35:48 -0300

    Initial version

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,26 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang='ts'>
+  import './styles.css';
+
+  const LONGITUD_MAXIMA = 140
+
+  const claseEspacioRestante = (restantes: number): string =>
+    restantes > 10 ? 'ok' : (restantes > 5 ? 'limite' : 'pasado')
+
+  let tweet = $state('')
+  let restantes = $derived(LONGITUD_MAXIMA - tweet.length)
+  let espacioRestanteClass = $derived(claseEspacioRestante(restantes))
+</script>
+
+<article class="container">
+  <h1>Twitter - Svelte</h1>
+  <form name="form" class="form">
+    <div>
+      <textarea id="texto" name="texto" data-testid="texto" placeholder="¿Qué estás pensando?" bind:value={tweet}></textarea>
+    </div>
+    <div>
+      <span data-testid="restantes" class="badge {espacioRestanteClass}">
+        {restantes}
+      </span>
+    </div>
+  </form>
+</article>
